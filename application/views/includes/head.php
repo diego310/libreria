c7f5bc9f911e4f1fcdec28b5e3a8eacc68753715
diego310 <diegoamarillas@gmail.com>
2015-08-29 12:51:30 -0700

    Update head.php

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -10,7 +10,7 @@
     <title>Libreria ISEI</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/public/css/simplex.css" rel="stylesheet">
+    <link href="/public/css/flatly.css" rel="stylesheet">
 
     <!-- Custom CSS -->
     <link href="/public/css/shop-homepage.css" rel="stylesheet">
